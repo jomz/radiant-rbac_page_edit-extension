de021a46356f6c5465030c58af1736e44381121b
joel <joel.oliveira@gmail.com>
2010-11-24 13:24:53 -0500

    expanded condition to check that the methods actually exist first

--- lib/pages_controller_role_extensions.rb
+++ lib/pages_controller_role_extensions.rb
@@ -7,8 +7,7 @@ module PagesControllerRoleExtensions
         :denied_message => "You aren't in an appropriate role for editing or adding children to that page."
 
       def user_is_in_page_role
-        return true if current_user.admin? || current_user.developer? || current_user.designer?
-  
+        return true if current_user.admin? || (current_user.respond_to?(:developer?) && current_user.developer?) || (current_user.respond_to?(:designer?) && current_user.designer?)
         page = Page.find(params[:id] || params[:page_id] || params[:parent_id])
   
         until page.nil? do
